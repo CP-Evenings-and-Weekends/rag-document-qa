61c504138c0ebc2d145e69881a6b8adce48a5342
Steph Asauskas <stephaniebarlowmodel@gmail.com>
2026-09-22 17:18:15 -0700

    Migrations

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
-
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -86,7 +85,7 @@ DATABASES = {
         'NAME': 'study_assistant',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
